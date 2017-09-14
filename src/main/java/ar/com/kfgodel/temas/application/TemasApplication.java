8f88474ddaeb1e7dd1e55f8076239562b15c4879
feche <fecheromero@gmail.com>
2017-09-14 11:07:36 -0300

    Acomodando un poco el TestAplication

--- src/main/java/ar/com/kfgodel/temas/application/TemasApplication.java
+++ src/main/java/ar/com/kfgodel/temas/application/TemasApplication.java
@@ -5,7 +5,6 @@ import ar.com.kfgodel.dependencies.api.DependencyInjector;
 import ar.com.kfgodel.dependencies.impl.DependencyInjectorImpl;
 import ar.com.kfgodel.orm.api.HibernateOrm;
 import ar.com.kfgodel.orm.api.config.DbCoordinates;
-import ar.com.kfgodel.orm.api.operations.basic.Save;
 import ar.com.kfgodel.orm.impl.HibernateFacade;
 import ar.com.kfgodel.temas.application.initializers.InicializadorDeDatos;
 import ar.com.kfgodel.temas.config.TemasConfiguration;
@@ -13,21 +12,12 @@ import ar.com.kfgodel.transformbyconvention.api.TypeTransformer;
 import ar.com.kfgodel.transformbyconvention.impl.B2BTransformer;
 import ar.com.kfgodel.transformbyconvention.impl.config.TransformerConfigurationByConvention;
 import ar.com.kfgodel.webbyconvention.api.WebServer;
-import ar.com.kfgodel.webbyconvention.api.auth.WebCredential;
 import ar.com.kfgodel.webbyconvention.api.config.WebServerConfiguration;
 import ar.com.kfgodel.webbyconvention.impl.JettyWebServer;
 import ar.com.kfgodel.webbyconvention.impl.config.ConfigurationByConvention;
-import convention.persistent.Reunion;
-import convention.persistent.Usuario;
-import convention.rest.api.tos.BackofficeUserTo;
-import convention.rest.api.tos.ReunionTo;
-import convention.rest.api.tos.UserTo;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.inject.Inject;
-import java.util.Optional;
-import java.util.function.Function;
 
 /**
  * This type represents the whole application as a single object.<br>
@@ -36,105 +26,107 @@ import java.util.function.Function;
  * Created by kfgodel on 22/03/15.
  */
 public class TemasApplication implements Application {
-  public static Logger LOG = LoggerFactory.getLogger(TemasApplication.class);
+    public static Logger LOG = LoggerFactory.getLogger(TemasApplication.class);
 
-  private TemasConfiguration config;
-  private DependencyInjector injector;
+    private TemasConfiguration config;
 
-  @Override
-  public WebServer getWebServerModule() {
-    return this.injector.getImplementationFor(WebServer.class).get();
-  }
+    @Override
+    public WebServer getWebServerModule() {
+        return this.getInjector().getImplementationFor(WebServer.class).get();
+    }
 
-  @Override
-  public HibernateOrm getOrmModule() {
-    return this.injector.getImplementationFor(HibernateOrm.class).get();
-  }
+    @Override
+    public HibernateOrm getOrmModule() {
+        return this.getInjector().getImplementationFor(HibernateOrm.class).get();
+    }
 
-  @Override
-  public TypeTransformer getTransformerModule() {
-    return this.injector.getImplementationFor(TypeTransformer.class).get();
-  }
+    @Override
+    public TypeTransformer getTransformerModule() {
+        return this.getInjector().getImplementationFor(TypeTransformer.class).get();
+    }
 
-  @Override
-  public TemasConfiguration getConfiguration() {
-    return config;
-  }
+    @Override
+    public TemasConfiguration getConfiguration() {
+        return config;
+    }
 
-  @Override
-  public DependencyInjector getInjector() {
-    return injector;
-  }
+    public void setConfiguration(TemasConfiguration aConfiguration) {
+        config = aConfiguration;
+    }
 
-  public static Application create(TemasConfiguration config) {
-    TemasApplication application = new TemasApplication();
-    application.config = config;
-    application.injector=config.getInjector();
-    return application;
-  }
+    @Override
+    public DependencyInjector getInjector() {
+        return config.getInjector();
+    }
 
-  /**
-   * Initializes application components and starts the server to listen for requests
-   */
-  @Override
-  public void start() {
-    LOG.info("Starting APP");
-    this.initialize();
-    this.getWebServerModule().startAndJoin();
-  }
 
-  @Override
-  public void stop() {
-    LOG.info("Stopping APP");
-    this.getWebServerModule().stop();
-    this.getOrmModule().close();
-  }
+    public static Application create(TemasConfiguration config) {
+        TemasApplication application = new TemasApplication();
+        application.setConfiguration(config);
+        return application;
+    }
 
-  private void initialize() {
-    this.injector = config.getInjector();
-    this.injector.bindTo(Application.class, this);
+    /**
+     * Initializes application components and starts the server to listen for requests
+     */
+    @Override
+    public void start() {
+        LOG.info("Starting APP");
+        this.initialize();
+        this.getWebServerModule().startAndJoin();
+    }
 
-    this.injector.bindTo(HibernateOrm.class, createPersistenceLayer());
-    // Web server depends on hibernate, so it needs to be created after hibernate
-    this.injector.bindTo(WebServer.class, createWebServer());
-    this.injector.bindTo(TypeTransformer.class, createTransformer());
+    @Override
+    public void stop() {
+        LOG.info("Stopping APP");
+        this.getWebServerModule().stop();
+        this.getOrmModule().close();
+    }
 
-    registerCleanupHook();
+    protected void initialize() {
+        this.getInjector().bindTo(Application.class, this);
 
-    InicializadorDeDatos.create(this).inicializar();
-  }
+        this.getInjector().bindTo(HibernateOrm.class, createPersistenceLayer());
+        // Web server depends on hibernate, so it needs to be created after hibernate
+        this.getInjector().bindTo(WebServer.class, createWebServer());
+        this.getInjector().bindTo(TypeTransformer.class, createTransformer());
 
-  protected TypeTransformer createTransformer() {
-    TransformerConfigurationByConvention configuration = TransformerConfigurationByConvention.create(this.getInjector());
-    return B2BTransformer.create(configuration);
-  }
+        registerCleanupHook();
 
-  private void registerCleanupHook() {
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      LOG.info("Cleaning-up before shutdown");
-      this.stop();
-    }, "cleanup-thread"));
-  }
+        InicializadorDeDatos.create(this).inicializar();
+    }
 
-  protected HibernateOrm createPersistenceLayer() {
-    DbCoordinates dbCoordinates = config.getDatabaseCoordinates();
-    HibernateOrm hibernateOrm = HibernateFacade.createWithConventionsFor(dbCoordinates);
-    return hibernateOrm;
-  }
+    protected TypeTransformer createTransformer() {
+        TransformerConfigurationByConvention configuration = TransformerConfigurationByConvention.create(this.getInjector());
+        return B2BTransformer.create(configuration);
+    }
 
-  protected WebServer createWebServer() {
-    WebServerConfiguration serverConfig = ConfigurationByConvention.create()
-      .authenticatingWith(config.autenticador())
-      .listeningHttpOn(config.getHttpPort())
-      .withInjections((binder) -> {
-        //Make application the only jetty injectable dependency
-        binder.bind(this).to(Application.class);
-      })
-      .withSecuredRootPaths("/api/v1")
-      .redirectingAfterSuccessfulAuthenticationTo("/")
-      .redirectingAfterFailedAuthenticationTo("/login?failed=true");
-    return JettyWebServer.createFor(serverConfig);
-  }
+    private void registerCleanupHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            LOG.info("Cleaning-up before shutdown");
+            this.stop();
+        }, "cleanup-thread"));
+    }
+
+    protected HibernateOrm createPersistenceLayer() {
+        DbCoordinates dbCoordinates = config.getDatabaseCoordinates();
+        HibernateOrm hibernateOrm = HibernateFacade.createWithConventionsFor(dbCoordinates);
+        return hibernateOrm;
+    }
+
+    protected WebServer createWebServer() {
+        WebServerConfiguration serverConfig = ConfigurationByConvention.create()
+                .authenticatingWith(config.autenticador())
+                .listeningHttpOn(config.getHttpPort())
+                .withInjections((binder) -> {
+                    //Make application the only jetty injectable dependency
+                    binder.bind(this).to(Application.class);
+                })
+                .withSecuredRootPaths("/api/v1")
+                .redirectingAfterSuccessfulAuthenticationTo("/")
+                .redirectingAfterFailedAuthenticationTo("/login?failed=true");
+        return JettyWebServer.createFor(serverConfig);
+    }
 
 
 }
--- src/test/java/Persistence/TestApplication.java
+++ src/test/java/Persistence/TestApplication.java
@@ -1,55 +1,12 @@
 package Persistence;
 
-import ar.com.kfgodel.dependencies.api.DependencyInjector;
-import ar.com.kfgodel.orm.api.HibernateOrm;
-import ar.com.kfgodel.orm.api.config.DbCoordinates;
-import ar.com.kfgodel.orm.impl.HibernateFacade;
 import ar.com.kfgodel.temas.application.Application;
 import ar.com.kfgodel.temas.application.TemasApplication;
-import ar.com.kfgodel.temas.application.initializers.InicializadorDeDatos;
 import ar.com.kfgodel.temas.config.TemasConfiguration;
-import ar.com.kfgodel.transformbyconvention.api.TypeTransformer;
-import ar.com.kfgodel.transformbyconvention.impl.B2BTransformer;
-import ar.com.kfgodel.transformbyconvention.impl.config.TransformerConfigurationByConvention;
-import ar.com.kfgodel.webbyconvention.api.WebServer;
-import ar.com.kfgodel.webbyconvention.api.config.WebServerConfiguration;
-import ar.com.kfgodel.webbyconvention.impl.JettyWebServer;
-import ar.com.kfgodel.webbyconvention.impl.config.ConfigurationByConvention;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Created by sandro on 19/06/17.
  */
-public class TestApplication implements Application{
-    public static Logger LOG = LoggerFactory.getLogger(TemasApplication.class);
-
-    private TemasConfiguration config;
-    private DependencyInjector injector;
-    @Override
-    public WebServer getWebServerModule() {
-        return this.injector.getImplementationFor(WebServer.class).get();
-    }
-
-    @Override
-    public HibernateOrm getOrmModule() {
-        return this.injector.getImplementationFor(HibernateOrm.class).get();
-    }
-
-    @Override
-    public TypeTransformer getTransformerModule() {
-        return this.injector.getImplementationFor(TypeTransformer.class).get();
-    }
-
-    @Override
-    public TemasConfiguration getConfiguration() {
-        return config;
-    }
-
-    @Override
-    public DependencyInjector getInjector() {
-        return injector;
-    }
+public class TestApplication extends TemasApplication{
 
 
     @Override
@@ -57,58 +14,11 @@ public class TestApplication implements Application{
         this.initialize();
     }
 
-    @Override
-    public void stop() {
-        this.getOrmModule().close();
-    }
-
     public static Application create(TemasConfiguration config) {
         TestApplication application = new TestApplication();
-        application.config = config;
+        application.setConfiguration(config);
         return application;
     }
 
-    protected TypeTransformer createTransformer() {
-        TransformerConfigurationByConvention configuration = TransformerConfigurationByConvention.create(this.getInjector());
-        return B2BTransformer.create(configuration);
-    }
 
-    private void registerCleanupHook() {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            LOG.info("Cleaning-up before shutdown");
-            this.stop();
-        }, "cleanup-thread"));
-    }
-
-    protected HibernateOrm createPersistenceLayer() {
-        DbCoordinates dbCoordinates = config.getDatabaseCoordinates();
-        HibernateOrm hibernateOrm = HibernateFacade.createWithConventionsFor(dbCoordinates);
-        return hibernateOrm;
-    }
-
-    protected WebServer createWebServer() {
-        WebServerConfiguration serverConfig = ConfigurationByConvention.create()
-                .authenticatingWith(config.autenticador())
-                .listeningHttpOn(config.getHttpPort())
-                .withInjections((binder) -> {
-                    //Make application the only jetty injectable dependency
-                    binder.bind(this).to(Application.class);
-                })
-                .withSecuredRootPaths("/api/v1")
-                .redirectingAfterSuccessfulAuthenticationTo("/")
-                .redirectingAfterFailedAuthenticationTo("/login?failed=true");
-        return JettyWebServer.createFor(serverConfig);
-    }
-
-    private void initialize() {
-        this.injector = config.getInjector();
-        this.injector.bindTo(Application.class, this);
-        this.injector.bindTo(WebServer.class, createWebServer());
-        this.injector.bindTo(HibernateOrm.class, createPersistenceLayer());
-        this.injector.bindTo(TypeTransformer.class, createTransformer());
-
-        registerCleanupHook();
-
-        InicializadorDeDatos.create(this).inicializar();
-    }
 }
